41eee74b97182d814157cc8d465c2a135b803a22
Heldeg <edhernandezfo@unal.edu.co>
2019-09-23 19:30:23 -0500

    validation

--- trip-planner/grails-app/domain/trip/planner/Trip.groovy
+++ trip-planner/grails-app/domain/trip/planner/Trip.groovy
@@ -1,7 +1,14 @@
 package trip.planner
 
 class Trip {
-
+    String title
+    String destination
+    String description
+    int value
     static constraints = {
+        title size: 5..15, blank: false, unique: true
+        destination size: 3..30, blank: false
+        description blank: true
+        value min: 1
     }
 }
